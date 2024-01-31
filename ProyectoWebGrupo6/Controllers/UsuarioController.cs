5b96347554c93aa64d41508fd68d50590ed4d520
Fabian-Montero <fabianja0477@gmail.com>
2024-01-30 18:53:05 -0600

    Commit inicial

---
+++ ProyectoWebGrupo6/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoWebGrupo6.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        public ActionResult OlvidarContrasenna()
+        {
+            return View();
+        }
+
+        public ActionResult Error401()
+        {
+            return View();
+        }
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        public ActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
