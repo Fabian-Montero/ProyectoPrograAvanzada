0163afaf7d7f3131f40f1e67bb56ae441eabc745
CharlieAcl <CharlieCalderon003@gmail.com>
2024-04-19 11:36:12 -0600

    Pedidos

--- ProyectoApi/ProyectoApiGupo6/ProyectoApiGupo6/Entidades/Carrito.cs
+++ ProyectoApi/ProyectoApiGupo6/ProyectoApiGupo6/Entidades/Carrito.cs
@@ -10,13 +10,16 @@ namespace ProyectoApiGupo6.Entidades
         public long Carritoid { get; set; }
         public long Usuarioid { get; set; }
         public long Productoid { get; set; }
-        public DateTime FechaCarrito { get; set; }
+        public DateTime Fecha { get; set; }
         public int Cantidad { get; set; }
         public decimal SubTotal { get; set; }
         public decimal Impuesto { get; set; }
         public decimal Total { get; set; }
         public decimal Precio { get; set; }
         public string Nombre { get; set; }
+        public long MaestroId { get; set; }
+        public long DetalleId { get; set; }
+        public bool Estado { get; set; }
     }
 
     public class ConfirmacionCarrito
--- ProyectoWebGrupo6/Models/CarritoModel.cs
+++ ProyectoWebGrupo6/Models/CarritoModel.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Security.Policy;
 using System.Web;
 
 namespace ProyectoWebGrupo6.Models
@@ -61,6 +62,77 @@ namespace ProyectoWebGrupo6.Models
                 {
                     return null;
                 }
+            }
+        }
+
+        public Confirmacion PagoCarrito(Carrito entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlWebApi"] + "Carrito/PagoCarrito";
+                JsonContent jsonEntidad = JsonContent.Create(entidad);
+                var respuesta = client.PostAsync(url, jsonEntidad).Result;
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<Confirmacion>().Result;
+                else
+                    return null;
+            }
+        }
+
+        public ConfirmacionCarrito ConsultarPedidos(long usuarioId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlWebApi"] + "Carrito/ConsultarPedidos?usuarioId=" + usuarioId;
+                var respuesta = client.GetAsync(url).Result;
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<ConfirmacionCarrito>().Result;
+                else
+                    return null;
+            }
+        }
+
+        public ConfirmacionCarrito ConsultarPedidosMantenimiento(bool MostrarTodos)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlWebApi"] + "Carrito/ConsultarPedidosMantenimiento?MostrarTodos=" + MostrarTodos;
+                var respuesta = client.GetAsync(url).Result;
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<ConfirmacionCarrito>().Result;
+                else
+                    return null;
+            }
+        }
+
+        public ConfirmacionCarrito ConsultarDetallePedidos(long maestroId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlWebApi"] + "Carrito/ConsultarDetallePedidos?maestroId=" + maestroId;
+                var respuesta = client.GetAsync(url).Result;
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<ConfirmacionCarrito>().Result;
+                else
+                    return null;
+            }
+        }
+
+        public Confirmacion ActualizarEstadoPedido(long maestroId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = ConfigurationManager.AppSettings["urlWebApi"] + "Carrito/ActualizarEstadoPedido?maestroId=" + maestroId;
+                var respuesta = client.DeleteAsync(url).Result;
+
+                if (respuesta.IsSuccessStatusCode)
+                    return respuesta.Content.ReadFromJsonAsync<Confirmacion>().Result;
+                else
+                    return null;
             }
         }
     }
